23619ba02887549f4fd77a9e7627295244a8ad92
softmoca <acyc0909@gmail.com>
2023-08-24 20:42:26 +0900

    feat: sequelizse database setting

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const pageRouter = require("./routes/page");
-
+const { sequelize } = require("./models");
 const app = express();
 app.set("port", process.env.PORT || 8001);
 app.set("view engine", "html");
@@ -16,7 +16,14 @@ nunjucks.configure("views", {
   express: app,
   watch: true,
 });
-
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("데이터베이스 연결 성공");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
