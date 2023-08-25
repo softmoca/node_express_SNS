8186da776ad0bb75cd819abf2126352877065899
softmoca <acyc0909@gmail.com>
2023-08-25 16:29:52 +0900

    feat : img upload

--- controllers/page.js
+++ controllers/page.js
@@ -1,15 +1,28 @@
+const { User, Post } = require("../models");
+
 exports.renderProfile = (req, res) => {
-  res.render('profile', { title: '내 정보 - NodeBird' });
+  res.render("profile", { title: "내 정보 - NodeBird" });
 };
 
 exports.renderJoin = (req, res) => {
-  res.render('join', { title: '회원가입 - NodeBird' });
+  res.render("join", { title: "회원가입 - NodeBird" });
 };
 
-exports.renderMain = (req, res, next) => {
-  const twits = [];
-  res.render('main', {
-    title: 'NodeBird',
-    twits,
-  });
+exports.renderMain = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ["id", "nick"],
+      },
+      order: [["createdAt", "DESC"]],
+    });
+    res.render("main", {
+      title: "NodeBird",
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 };
