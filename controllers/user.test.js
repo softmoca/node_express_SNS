03814485067e09181fe9e478e99dc9a9d4c018cf
softmoca <acyc0909@gmail.com>
2023-08-29 22:06:33 +0900

    feat : Integration_Load test

--- controllers/user.test.js
+++ controllers/user.test.js
@@ -1,27 +1,39 @@
-const Sequelize = require("sequelize");
-const User = require("./user");
-const config = require("../config/config")["test"];
-const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config
-);
+jest.mock('../models/user');
+const User = require('../models/user');
+const { follow } = require('./user');
 
-describe("User 모델", () => {
-  test("static init 메서드 호출", () => {
-    expect(User.initiate(sequelize)).toBe(undefined);
+describe('follow', () => {
+  const req = {
+    user: { id: 1 },
+    params: { id: 2 },
+  };
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(),
+  };
+  const next = jest.fn();
+
+  test('사용자를 찾아 팔로잉을 추가하고 success를 응답해야 함', async () => {
+    User.findOne.mockReturnValue({
+      addFollowing(id) {
+        return Promise.resolve(true);
+      }
+    });
+    await follow(req, res, next);
+    expect(res.send).toBeCalledWith('success');
   });
-  test("static associate 메서드 호출", () => {
-    const db = {
-      User: {
-        hasMany: jest.fn(),
-        belongsToMany: jest.fn(),
-      },
-      Post: {},
-    };
-    User.associate(db);
-    expect(db.User.hasMany).toHaveBeenCalledWith(db.Post);
-    expect(db.User.belongsToMany).toHaveBeenCalledTimes(2);
+
+  test('사용자를 못 찾으면 res.status(404).send(no user)를 호출함', async () => {
+    User.findOne.mockReturnValue(null);
+    await follow(req, res, next);
+    expect(res.status).toBeCalledWith(404);
+    expect(res.send).toBeCalledWith('no user');
+  });
+
+  test('DB에서 에러가 발생하면 next(error) 호출함', async () => {
+    const message = 'DB에러';
+    User.findOne.mockReturnValue(Promise.reject(message));
+    await follow(req, res, next);
+    expect(next).toBeCalledWith(message);
   });
 });
