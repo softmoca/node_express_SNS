b635ecf8dec01ddcade3eaaadd90a988a07699bb
softmoca <acyc0909@gmail.com>
2023-08-31 16:19:20 +0900

    feat : s3_lamda

--- controllers/post.js
+++ controllers/post.js
@@ -1,8 +1,8 @@
-const { Post, Hashtag } = require('../models');
+const { Post, Hashtag } = require("../models");
 
 exports.afterUploadImage = (req, res) => {
   console.log(req.file);
-  res.json({ url: `/img/${req.file.filename}` });
+  res.json({ url: req.file.location });
 };
 
 exports.uploadPost = async (req, res, next) => {
@@ -15,15 +15,15 @@ exports.uploadPost = async (req, res, next) => {
     const hashtags = req.body.content.match(/#[^\s#]*/g);
     if (hashtags) {
       const result = await Promise.all(
-        hashtags.map(tag => {
+        hashtags.map((tag) => {
           return Hashtag.findOrCreate({
             where: { title: tag.slice(1).toLowerCase() },
-          })
-        }),
+          });
+        })
       );
-      await post.addHashtags(result.map(r => r[0]));
+      await post.addHashtags(result.map((r) => r[0]));
     }
-    res.redirect('/');
+    res.redirect("/");
   } catch (error) {
     console.error(error);
     next(error);
--- routes/post.js
+++ routes/post.js
@@ -2,9 +2,12 @@ const express = require("express");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
+const { S3Client } = require("@aws-sdk/client-s3");
+const multerS3 = require("multer-s3");
 
 const { afterUploadImage, uploadPost } = require("../controllers/post");
 const { isLoggedIn } = require("../middlewares");
+
 const router = express.Router();
 
 try {
@@ -14,14 +17,19 @@ try {
   fs.mkdirSync("uploads");
 }
 
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  },
+  region: "ap-northeast-2",
+});
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, cb) {
-      cb(null, "uploads/");
-    },
-    filename(req, file, cb) {
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
+  storage: multerS3({
+    s3,
+    bucket: "nodebird03",
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${file.originalname}`);
     },
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
